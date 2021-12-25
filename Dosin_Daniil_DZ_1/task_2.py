2c449c41e83b3544ce52b2f7c41fae31ee62255b
daniildn <dosindaniilm@gmail.com>
2021-12-25 23:35:24 +0300

    completed the second task

--- Dosin_Daniil_DZ_1/task_2.py
+++ Dosin_Daniil_DZ_1/task_2.py
@@ -1,35 +1,43 @@
 # TODO Создать список, состоящий из кубов нечётных чисел от 1 до 1000 (куб X - третья степень числа X):
-# a. Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7. Например, число «19 ^ 3 = 6859»
+# + a. Вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7. Например, число «19 ^ 3 = 6859»
 # будем включать в сумму, так как 6 + 8 + 5 + 9 = 28 – делится нацело на 7. Внимание: использовать только арифметические операции!
-# b. К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7.
+# + b. К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7.
 
-# * Решить задачу под пунктом b, не создавая новый список.
+# + * Решить задачу под пунктом b, не создавая новый список.
 
-input_list = [n ** 3 for n in range(0, 1000) if n % 2 != 0]
-print(input_list)
 
-result = 0
-for val in input_list:
-    amount = 0
-    n = val
-    while n > 0:
-        dig = n % 10
-        amount += dig
-        n = n // 10
-    if amount % 7 == 0:
-        result += val
-print(result)
+def return_list_cubes():
+    return [n ** 3 for n in range(0, 1000) if n % 2 != 0]
 
-input_list = [n+17 for n in input_list]
 
-result = 0
-for val in input_list:
-    amount = 0
-    n = val
-    while n > 0:
-        dig = n % 10
-        amount += dig
-        n = n // 10
-    if amount % 7 == 0:
-        result += val
-print(result)
+def sum_div_7(in_list: list):
+    res = 0
+    for val in in_list:
+        sum = 0
+        n = val
+        while n > 0:
+            dig = n % 10
+            sum += dig
+            n = n // 10
+        if sum % 7 == 0:
+            res += val
+    return res
+
+
+def sum_div_7_plus_17(in_list: list):
+    in_list = [n + 17 for n in in_list]
+    res = 0
+    for val in in_list:
+        sum = 0
+        n = val
+        while n > 0:
+            dig = n % 10
+            sum += dig
+            n = n // 10
+        if sum % 7 == 0:
+            res += val
+    return res
+
+
+print(sum_div_7(return_list_cubes()))
+print(sum_div_7_plus_17(return_list_cubes()))
