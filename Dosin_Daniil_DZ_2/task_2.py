7b08d866ae52d2dca314c1121607b2a07c41888e
daniildn <dosindaniilm@gmail.com>
2021-12-30 01:37:53 +0300

    completed the second task)))

--- Dosin_Daniil_DZ_2/task_2.py
+++ Dosin_Daniil_DZ_2/task_2.py
@@ -1,18 +1,43 @@
-input_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-# ['в', '"', '05', '"', 'часов', '"', '17', '"', 'минут', 'температура', 'воздуха', 'была', '"', '+5', '"', 'градусов']
-
 # todo обособить каждое целое число (вещественные не трогаем) кавычками
 # (добавить кавычку до и кавычку после элемента списка, являющегося числом)
 # и дополнить нулём до двух целочисленных разрядов
-index_of_int_list = [n for n in range(len(input_list)) if input_list[n].strip('=+-;:"\'').isdigit()]
-for ind in reversed(index_of_int_list):
-    input_list.insert(ind, '"')
-    input_list.insert(ind + 2, '"')
-for i in range(len(input_list)):
-    element = input_list[i]
-    if element.strip('=+-;:"\'').isdigit():
-        if not element[0].isdigit() and int(element.strip('=+-;:"\'')) < 10:
-            input_list[i] = '{}0{}'.format(element[0], int(element.strip('=+-;:"\'')))
-        elif int(element.strip('=+-;:"\'')) < 10:
-            input_list[i] = '0{}'.format(int(element.strip('=+-;:"\'')))
-print(input_list)
+# from io import StringIO
+
+
+# WORK IN PLACE
+def convert_list_in_str(list_in: list) -> str:
+    """Обособляет каждое целое число кавычками, добавляя кавычку до и после элемента
+        списка, являющегося числом, и дополняет нулём до двух целочисленных разрядов.
+        Формирует из списка результирующую строковую переменную и возвращает."""
+    index_of_int_list = [n for n in range(len(list_in)) if list_in[n].strip('=+-;:"\'').isdigit()]
+    for ind in reversed(index_of_int_list):
+        list_in.insert(ind, '"')
+        list_in.insert(ind + 2, '"')
+    # ноль перед одностоящим числом
+    for i in range(len(list_in)):
+        element = list_in[i]
+        print(element)
+        if element.strip('=+-;:"\'').isdigit():
+            if not element[0].isdigit() and int(element.strip('=+-;:"\'')) < 10:
+                list_in[i] = '{}0{}'.format(element[0], int(element.strip('=+-;:"\'')))
+
+            elif int(element.strip('=+-;:"\'')) < 10:
+                list_in[i] = '0{}'.format(int(element.strip('=+-;:"\'')))
+    # CONVERT LIST TO STRING
+    str_out_list = []
+    i = 0
+    while i < len(list_in):
+        element = list_in[i]
+        if i < len(list_in) - 2 and (element in ["'", '"'] and list_in[i + 1].strip('+-').isdigit()):
+            str_out_list.append(f"{element}")
+            str_out_list.append(f"{list_in[i + 1]}")
+            i += 2
+        else:
+            str_out_list.append(f"{element} ")
+            i += 1
+    return ''.join(str_out_list)
+
+
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+50', 'градусов']
+result = convert_list_in_str(my_list)
+print(result)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python_basics_main_course
 main course of python language basics
+
+я так понял, что обычная string-concat  не очень эффективна, 
+но можно использовать stringIO или даже join() метод, который на 40% эффективнее чем обычная
+string-concat
