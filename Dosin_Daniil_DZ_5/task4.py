805b839b928011869daf8c55e2773fb483f026c9
daniildn <dosindaniilm@gmail.com>
2022-01-17 21:21:33 +0300

    task 4 is completed

--- Dosin_Daniil_DZ_5/task4.py
+++ Dosin_Daniil_DZ_5/task4.py
@@ -9,15 +9,15 @@ def get_numbers(src: list):
             yield src[i]
 
 
-src = [random.randint(0, 1009) for _ in range(10 ** 6)]
+src = [random.randint(0, 10009) for _ in range(10 ** 7)]
 
 start = time.perf_counter()
-print(*get_numbers(src))
+sum(get_numbers(src))
 time_1 = time.perf_counter() - start
 print(time_1)
 
 start = time.perf_counter()
 nums = [i for i in range(1, len(src)) if src[i] > src[i - 1]]
-print(nums)
+sum(nums)
 print("generator time is ", time_1)
 print("list comprehension time is", time.perf_counter() - start)
