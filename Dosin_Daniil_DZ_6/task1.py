949137898996b12d2914bbb743fbb3e31d69b351
daniil_dn <dosindaniilm@gmail.com>
2022-01-21 16:30:37 +0300

    refactored

--- Dosin_Daniil_DZ_6/task1.py
+++ Dosin_Daniil_DZ_6/task1.py
@@ -13,14 +13,16 @@ from pprint import pprint
 
 
 def get_parse_attrs(line: str) -> tuple:
-    """Парсит строку на атрибуты и возвращает кортеж атрибутов (<remote_addr>, <request_type>, <requested_resource>)"""
+    """Парсит строку на атрибуты и возвращает кортеж атрибутов (<remote_addr>, <request_type>, <requested_resource>)
+    :param line: line of nginx logs
+    :return tuple: return tuple like (<remote_addr>, <request_type>, <requested_resource>)
+    """
     line_list = line.split(' ')
     res = tuple([line_list[0], line_list[5].strip("'\""), line_list[6]])
     return res  # верните кортеж значений <remote_addr>, <request_type>, <requested_resource>
 
 
 list_out = list()
-spanners = dict()
 with open('nginx_logs.txt', 'r', encoding='utf-8') as fr:
     while True:
         line = fr.readline()
