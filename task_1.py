5d267dd9aba9142630024b3538ef7a68d7c7653c
daniil_dn <dosindaniilm@gmail.com>
2022-01-25 16:25:20 +0300

    head-on solution for task1

---
+++ task_1.py
@@ -0,0 +1,24 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def create_new_dir(name: str, path: str = '') -> str:
+    """
+    create a new directory in path and check existing
+    :param name: name of a new directory
+    :param path: path for the new directory
+    :return: path for the new directory
+    """
+    if not os.path.exists(os.path.join(path, name)):
+        os.mkdir(os.path.join(path, name))
+        return os.path.join(path, name)
+    else:
+        return os.path.join(path, name)
+
+
+path_new = create_new_dir('my_project', BASE_DIR)
+create_new_dir('settings', path_new)
+create_new_dir('mainapp', path_new)
+create_new_dir('adminapp', path_new)
+create_new_dir('authapp', path_new)
