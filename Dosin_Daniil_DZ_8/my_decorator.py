e72034c4b643ec6ece1894db63c20373d378dadc
daniil_dn <dosindaniilm@gmail.com>
2022-01-29 12:01:21 +0300

    my own decorator

--- Dosin_Daniil_DZ_8/my_decorator.py
+++ Dosin_Daniil_DZ_8/my_decorator.py
@@ -10,4 +10,25 @@ def say_hi(word):
     print(f"HI, {word}")
 
 
-say_hi = my_decorator(say_hi)('ss')
+# # say_hi = my_decorator(say_hi)('ss')
+
+def my_decorator_args(decorator_arg):
+    print(decorator_arg)
+
+    def _my_decorator_args(func):
+        def wrapper(word):
+            func(word)
+            print('decorator')
+
+        return wrapper
+
+    return _my_decorator_args
+
+
+def say_word(word):
+    print(f"HI, {word}")
+
+
+say_word = my_decorator_args('docorator\'s arg')(say_word)
+say_word('s')
+
